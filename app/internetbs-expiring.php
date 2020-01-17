873ebcd140f202fec6696a985d6883191e8a22d5
davidofug <davidwampamba@gmail.com>
2020-01-18 00:14:49 +0300

    vlucas/phpdotenv added

--- app/internetbs-expiring.php
+++ app/internetbs-expiring.php
@@ -3,9 +3,9 @@ require_once '../vendor/autoload.php';
 
 //echo 'Starting script<br/>';
 
-//error_reporting(E_ALL);
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 //logToEmail($email='davidwampamba@gmail.com');
 
